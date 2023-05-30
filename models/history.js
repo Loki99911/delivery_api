7b317d71984299bbd7d3fdb3174bba82f94afb98
Loki99911 <nikita_kresik@i.ua>
2023-05-30 13:19:16 +0300

    Schema fix

--- models/history.js
+++ models/history.js
@@ -1,5 +1,79 @@
 const { Schema, model } = require('mongoose');
 
+// const historySchema = Schema(
+//   {
+//     name: {
+//       type: String,
+//       required: true,
+//     },
+//     email: {
+//       type: String,
+//       required: true,
+//     },
+//     phone: {
+//       type: String,
+//       required: true,
+//     },
+//     adress: {
+//       type: String,
+//       required: true,
+//     },
+//     totalValue: {
+//       type: String,
+//       required: true,
+//     },
+//     meals: {
+//       type: Array,
+//       required: true,
+//       name: {
+//         type: String,
+//         required: true,
+//       },
+//       price: {
+//         type: String,
+//         required: true,
+//       },
+//       itemId: {
+//         type: String,
+//         required: true,
+//       },
+//       quantity: {
+//         type: String,
+//         required: true,
+//       },
+//       photo: {
+//         type: String,
+//         required: true,
+//       },
+//     },
+//   },
+
+//   { timestamps: true, versionKey: false }
+// );
+
+const mealSchema = Schema({
+  itemId: {
+    type: String,
+    required: true,
+  },
+  name: {
+    type: String,
+    required: true,
+  },
+  price: {
+    type: String,
+    required: true,
+  },
+  quantity: {
+    type: String,
+    required: true,
+  },
+  photo: {
+    type: String,
+    required: true,
+  },
+});
+
 const historySchema = Schema(
   {
     name: {
@@ -14,7 +88,7 @@ const historySchema = Schema(
       type: String,
       required: true,
     },
-    adress: {
+    address: {
       type: String,
       required: true,
     },
@@ -23,33 +97,13 @@ const historySchema = Schema(
       required: true,
     },
     meals: {
-      type: Array,
+      type: [mealSchema],
       required: true,
-      name: {
-        type: String,
-        required: true,
-      },
-      price: {
-        type: String,
-        required: true,
-      },
-      itemId: {
-        type: String,
-        required: true,
-      },
-      quantity: {
-        type: String,
-        required: true,
-      },
-      photo: {
-        type: String,
-        required: true,
-      },
     },
   },
-
   { timestamps: true, versionKey: false }
 );
+
 
 const History = model('History', historySchema);
 
